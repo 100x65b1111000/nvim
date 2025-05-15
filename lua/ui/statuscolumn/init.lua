ab7482c8c473bf64ef4a0953f255dd1cf64d39e6
Dex <hs8510842667@gmail.com>
2025-05-15 09:01:01 +0530

    statuscolumn: extmarks

--- lua/ui/statuscolumn/init.lua
+++ lua/ui/statuscolumn/init.lua
@@ -3,7 +3,6 @@ local M = {}
 M.setup = function ()
 	-- local utils = require('ui.statucolumn.utils')
 	vim.api.nvim_set_option_value('statuscolumn', "%!v:lua.require('ui.statuscolumn.utils').set_statuscolumn()", {})
-
 end
 
 return M
