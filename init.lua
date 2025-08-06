add54ab6229b18ee40d097b48576697d002430a5
Dex <hs8510842667@gmail.com>
2025-08-06 15:48:06 +0530

    misc(tabline): do not set hide_misc_buffers to true by default

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 require("imports")
 
 if vim.version().minor >= 12 then
-	vim.schedule(function ()
+	vim.schedule(function()
 		require("vim._extui").enable({ enable = true, msg = { target = "cmd", timeout = 5000 } })
 	end)
 end
--- lua/imports.lua
+++ lua/imports.lua
@@ -5,5 +5,6 @@ require("core.abbr")
 require("ui").setup({
 	enabled = true,
 	statusline = { enabled = true, left = { separator = { left = "", right = "" } } },
+	tabline = { enabled = true, hide_misc_buffers = true },
 })
 require("config.lsp")
